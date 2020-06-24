69f616b2a0c77320e90fe21ea29ba7d1bc285a14
AmitAmit <ameetchopade@gmail.com>
2020-06-25 00:14:05 +0800

    Update install-apache.sh

--- install-apache.sh
+++ install-apache.sh
@@ -10,4 +10,6 @@ sudo yum install -y mysql php php-mysql
 sudo yum update -y
 sudo yum-config-manager --enable epel
 sudo yum install ansible -y
+sudo git clone https://github.com/amitchopade/terraform/
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+sudo ansible-playbook main.yml
